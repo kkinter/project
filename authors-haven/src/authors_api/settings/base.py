3046db521779a40176aaa8e78f199a809313b6d3
kkinter <ifol1129@gmail.com>
2024-03-24 00:50:10 +0900

    feat: configure celery flower

--- authors-haven/src/authors_api/settings/base.py
+++ authors-haven/src/authors_api/settings/base.py
@@ -28,6 +28,7 @@ THIRD_PARTY_APPS = ["rest_framework",
                     "phonenumber_field",
                     "drf_yasg",
                     "corsheaders",
+                    "djcelery_email",
 ]
 
 LOCAL_APPS = ["core_apps.profiles", 
@@ -144,6 +145,17 @@ CORS_URLS_REGEX = r"^api/.*$"
 
 
 AUTH_USER_MODEL = "users.User"
+
+CELERY_BROKER_URL = env("CELERY_BROKER")
+CELERY_RESULT_BACKEND = CELERY_BROKER_URL
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
+CELERY_RESULT_BACKEND_MAX_RETRIES = 10
+CELERY_TASK_SEND_SENT_EVENT = True
+
+if USE_TZ:
+    CELERY_TIMEZONE = TIME_ZONE
 
 LOGGING = {
     "version": 1,
