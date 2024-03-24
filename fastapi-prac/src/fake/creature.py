50d4663ebc3f1223f5e90844a8322d99321ba984
kkinter <ifol1129@gmail.com>
2024-03-25 05:35:13 +0900

    chore: fastapi setup

---
+++ fastapi-prac/src/fake/creature.py
@@ -0,0 +1,42 @@
+from model.creature import Creature
+
+
+_creatures = [
+    Creature(name="Yeti",
+             aka="Abominable Snowman",
+             country="CN",
+             area="Himalayas",
+             description="Hirsute Himalayan"),
+    Creature(name="Bigfoot",
+             description="Yeti's Cousin Eddie",
+             country="US",
+             area="*",
+             aka="Sasquatch"),
+    ]
+
+def get_all() -> list[Creature]:
+    """Return all creatures"""
+    return _creatures
+
+def get_one(name: str) -> Creature | None:
+    """Return one creature"""
+    for _creature in _creatures:
+        if _creature.name == name:
+            return _creature
+    return None
+
+def create(creature: Creature) -> Creature:
+    """Add a creature"""
+    return creature
+
+def modify(creature: Creature) -> Creature:
+    """Partially modify a creature"""
+    return creature
+
+def replace(creature: Creature) -> Creature:
+    """Completely replace a creature"""
+    return creature
+
+def delete(name: str):
+    """Delete a creature; return None if it existed"""
+    return None
