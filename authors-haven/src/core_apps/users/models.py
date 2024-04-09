5397cd898d24ecd4902234340e5973c9bc27e57b
kkinter <ifol1129@gmail.com>
2024-04-10 05:08:03 +0900

    feat: add Profile model

--- authors-haven/src/core_apps/users/models.py
+++ authors-haven/src/core_apps/users/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
+
 from .managers import CustomUserManager
 
 
@@ -32,9 +33,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.first_name
-    
-    # property 있으면 user.get_full_name
-    # property 없으면 user.get_full_name()
+
     @property
     def get_full_name(self):
         return f"{self.first_name.title()} {self.last_name.title()}"
